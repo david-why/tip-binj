fba2f7b308e10c7e79b3db1718cedf0695088bee
david-why <david_why@outlook.com>
2025-11-06 14:53:40 +0800

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <UApp>
+    <NuxtLoadingIndicator color="oklch(70.5% 0.213 47.604)" />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
+</template>
---
+++ app/layouts/default.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const route = useRoute();
+
+const navItems = computed<NavigationMenuItem[]>(() => [
+  {
+    label: "Home",
+    to: "/",
+    icon: "i-material-symbols-home-outline",
+    active: route.path === "/",
+  },
+]);
+</script>
+
+<template>
+  <UHeader class="mb-4">
+    <template #title>
+      <div class="flex flex-col">
+        <span class="text-primary">TIP</span>
+        <span class="text-muted text-sm">Teacher Infraction Policy</span>
+      </div>
+    </template>
+
+    <UNavigationMenu :items="navItems" />
+
+    <template #body>
+      <UNavigationMenu
+        :items="navItems"
+        orientation="vertical"
+        class="-mx-2.5"
+      />
+    </template>
+  </UHeader>
+
+  <!-- eslint-disable-next-line vue/no-multiple-template-root -->
+  <UMain>
+    <UContainer>
+      <slot />
+    </UContainer>
+  </UMain>
+</template>
